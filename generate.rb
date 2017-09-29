fb03817ef3aa06b4d441bbd51a08d7aaa7fe3575
Benjamin Mosior <bemosior@gmail.com>
2017-09-29 19:05:37 -0400

    Read spreadsheet, print responses.

--- generate.rb
+++ generate.rb
@@ -1,9 +1,30 @@
 require 'erb'
+require 'spreadsheet'
+require 'pp'
+
+book = Spreadsheet.open('data.xls')
+sheet = book.worksheet(0)
+rows = []
+sheet.each do |row|
+  rows.push(row)
+end
+
+questions_row = rows.shift
+questions = []
+questions_row.each do |question|
+  questions.push question
+end
+
+responses = []
+rows.each do |row|
+  response = {}
+  row.each_with_index do |answer, index|
+    response[questions[index].to_s] = answer
+  end
+  responses.push response
+end
+
 
 template = File.read('./generate.erb')
-
-questions = ['Question 1', 'Question 2', 'Question 3']
-
 rendered = ERB.new(template).result(binding)
-
 File.write('./generated.html', rendered)
